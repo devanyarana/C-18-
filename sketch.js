5f97002c4fd2122fadf80ce145ee1be1b50010fe
devanyarana <91303751+devanyarana@users.noreply.github.com>
2021-12-01 20:27:21 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+
+function setup() {
+  createCanvas(400,400);
+s1 = new Student ("Avishi",12,7);
+s2 = new Student ("Rohan",14,8);
+
+s1.show();
+s2.show();
+}
+
+function draw() 
+{
+  background(30);
+
+  
+
+}
